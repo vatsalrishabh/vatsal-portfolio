7a587c9ab6d2bfeae48fffe5e1efa1087eb7b56b
DimpleKA <153168007+DimpleKA@users.noreply.github.com>
2024-02-24 02:04:37 +0530

    have writing

--- src/Components/Particle.js
+++ src/Components/Particle.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Particle = () => {
+    
   return (
-    <div>
-Particle 
-<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <>
+     <br/><br/><br/><br/><br/>
+    <div className='Particle'>
+   
+    <div className='Particle-left'>
+          <div className='Particle-autocomplete'>
+              <h1>Hi,</h1>
+              <div className='particle-h1-flex'>
+                <h1> I am </h1>
+                <h1 className='h1red'> Vatsal </h1>
+              </div>
+
+              <h1 className='typing-text'>I am a Developer.</h1>
+          </div>
     </div>
-  )
+    <div className='Particle-right'>
+     <img src='' alt='dpp'/>
+    </div>
+  </div>
+    
+    </>
+    
+  );
 }
 
-export default Particle
+export default Particle;
